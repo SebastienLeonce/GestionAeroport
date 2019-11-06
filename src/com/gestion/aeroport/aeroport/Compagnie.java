2ac3bbe3723b8ffc10b4ebb2f2187805b5891905
Sébastien LEONCE <sebastien.leonce@u-psud.fr>
2019-11-06 21:37:36 +0100

    data generator company

--- src/com/gestion/aeroport/aeroport/Compagnie.java
+++ src/com/gestion/aeroport/aeroport/Compagnie.java
@@ -6,7 +6,11 @@ import com.gestion.aeroport.passager.Pilote;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Compagnie {
 	
@@ -22,5 +26,43 @@ public class Compagnie {
 		this.flotte = flotte;
 		this.pilotes = pilotes;
 		this.personnels = personnels;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Compagnie: {" + "\n"+ 
+					"\tnom: " + nom + "\n"+ 
+					"\tnationalite: " + nationalite + "\n"+ 
+					"\tflotte=" + flotte + "\n"+ 
+					"\tpilotes=" + pilotes + "\n"+ 
+					"\tpersonnels=" + personnels + "\n"+ 
+				"}";
+	}
+
+
+	public static void main (String[] args) throws IOException {
+		URL url = new URL("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat");
+		Scanner sc = new Scanner(url.openStream()); 
+		
+		String[] data;
+		
+		List<Avion>  flotte = new ArrayList<Avion>();
+		List<Pilote> pilotes = new ArrayList<Pilote>();
+		List<Personnel> personnels = new ArrayList<Personnel>();
+		
+		Compagnie compagnie;
+		boolean flag = true;
+		
+		while (sc.hasNextLine()) {
+			data = sc.nextLine().split(","); 
+			
+			if (flag) {flag = false; continue;}
+			
+			compagnie = new Compagnie(data[1], data[6], flotte, pilotes, personnels);
+			System.out.println(compagnie.toString());
+		}
+		
+		sc.close();
 	}
 }
--- src/com/gestion/aeroport/avion/Avion.java
+++ src/com/gestion/aeroport/avion/Avion.java
@@ -1,18 +1,29 @@
 package com.gestion.aeroport.avion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import com.gestion.aeroport.passager.Passager;
+
 public abstract class Avion {
 	
 	private String modele;
-	private int capacite;
+	private int capaciteMax;
+	private int capacite = 0;
 	private float poidsBagagesMax;
+	private float poidsBagages = 0;
 	private float volCarburant;
-	private int nbPilotes;
+	private int nbPilotesMin;
+	private int nbPilotes = 0;
+	
+	private List<Passager> passagers = new ArrayList<Passager>();
 	
 	public Avion(String modele, int capacite, float poidsBagagesMax, float volCarburant, int nbPilotes) {
 		this.modele = modele;
-		this.capacite = capacite;
+		this.capaciteMax = capacite;
 		this.poidsBagagesMax = poidsBagagesMax;
 		this.volCarburant = volCarburant;
-		this.nbPilotes = nbPilotes;
+		this.nbPilotesMin = nbPilotes;
 	}
 }
