ace4118c45b139d7d2878cd4466bb4fa8377aeb2
Sebastien Leonce <sebastienleonce91@gmail.com>
2019-11-20 09:41:25 +0100

    doc

--- src/com/gestion/aeroport/passager/Personnel.java
+++ src/com/gestion/aeroport/passager/Personnel.java
@@ -8,20 +8,41 @@ public class Personnel extends Passager {
 	
 	private Compagnie compagnie;
 
+	/**
+	 * 
+	 * @param prenom
+	 * @param nom
+	 * @param anniversaire
+	 * @param nationalite
+	 * @param numeroPasseport
+	 * @param voyage
+	 * @param compagnie
+	 */
 	public Personnel(String prenom, String nom, Calendar anniversaire, String nationalite, String numeroPasseport, String voyage, Compagnie compagnie) {
 		super(prenom, nom, anniversaire, nationalite, numeroPasseport, voyage);
 		this.compagnie = compagnie;
 	}
 	
+	/**
+	 * 
+	 * @param compagnie
+	 */
 	public Personnel (Compagnie compagnie) {
 		super(null);
 		this.compagnie = compagnie;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Compagnie getCompagnie() {
 		return compagnie;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public String toString() {
 		return 	super.toString() + 
