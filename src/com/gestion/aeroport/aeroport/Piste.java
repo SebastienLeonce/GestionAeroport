e1951cec5312d1ef609ffb4b0abdd9bbc27b8323
lucas.payet <lucas.payet@u-psud.fr>
2019-12-02 19:22:57 +0100

    Ajout Piste et Vol

--- src/com/gestion/aeroport/aeroport/Piste.java
+++ src/com/gestion/aeroport/aeroport/Piste.java
@@ -1,5 +1,32 @@
 package com.gestion.aeroport.aeroport;
 
+import java.util.ArrayList;
+
 public class Piste {
+
+    public enum TypePiste{
+        DECOLLAGE, ATTERISSAGE
+    }
+
+    private ArrayList<Vol> fileDAttente;
+    private int espacement;
+    private TypePiste type;
+
+    Piste(final int espacement, TypePiste type){
+        this.fileDAttente = new ArrayList<Vol>();
+        this.espacement = espacement;
+        this.type = type;
+    }
+
+
+    public ArrayList<Vol> getFileDAttente(){
+        return this.fileDAttente;
+    }
+    public int getEspacement(){
+        return this.espacement;
+    }
+    public TypePiste getTypePiste(){
+        return this.type;
+    }
 
 }
--- src/com/gestion/aeroport/aeroport/Vol.java
+++ src/com/gestion/aeroport/aeroport/Vol.java
@@ -1,5 +1,34 @@
 package com.gestion.aeroport.aeroport;
 
+import java.util.ArrayList;
+
+import com.gestion.aeroport.avion.Avion;
+import com.gestion.aeroport.passager.Passager;
+
 public class Vol {
 
+    private Avion avion;
+    private ArrayList<Passager> occupants;
+    private Aeroport aeroportArrivee;
+    private Aeroport aeroportDepart;
+
+    public Vol(Avion avion, Aeroport arrivee, Aeroport depart){
+        this.occupants = new ArrayList<Passager>();
+        this.aeroportArrivee = arrivee;
+        this.aeroportDepart = depart;
+        this.avion = avion;
+    }
+
+    public Avion getAvion(){
+        return this.avion;
+    }
+    public ArrayList<Passager> getOccupants(){
+        return this.occupants;
+    }
+    public Aeroport getDepart(){
+        return this.aeroportDepart;
+    }
+    public Aeroport getArrivee(){
+        return this.aeroportArrivee;
+    }
 }
